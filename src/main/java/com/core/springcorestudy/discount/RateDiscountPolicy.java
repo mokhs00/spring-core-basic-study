fbf0ac1d7ab2fb5ab1ffb452f80387de7d9d0a8c
mokhs00 <mokhs00@naver.com>
2021-11-21 17:46:57 +0900

    add : RateDiscountPolicy

--- src/main/java/com/core/springcorestudy/discount/RateDiscountPolicy.java
+++ src/main/java/com/core/springcorestudy/discount/RateDiscountPolicy.java
@@ -3,18 +3,17 @@ package com.core.springcorestudy.discount;
 import com.core.springcorestudy.member.Grade;
 import com.core.springcorestudy.member.Member;
 
-public class FixDiscountPolicy implements DiscountPolicy {
+public class RateDiscountPolicy implements DiscountPolicy{
 
-    private int discountFixAmount = 1000;
+    private int discountPercent = 10;
 
     @Override
     public int discount(Member member, int price) {
 
         if (member.getGrade() == Grade.VIP) {
-            return discountFixAmount;
+            return price * discountPercent / 100;
         }
 
         return 0;
     }
-
 }
